27186c161ae435efcc66461ac57ba87ce2892efb
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2022-10-22 01:31:00 +0100

    changed sonnection mode

---
+++ config/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const development = {
+
+  database: process.env.PG_DATABASE,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  dialect: 'postgres'
+};
+
+const testing = {
+  database: 'databasename',
+  username: 'username',
+  password: 'password',
+  host: 'localhost',
+  dialect: 'sqlite'
+};
+
+const production = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql'
+};
+
+module.exports = {
+  development,
+  testing,
+  production
+};
