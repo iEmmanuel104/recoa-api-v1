eeab439370e2775d6d044cd1cae90779bbdfe34f
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2022-11-24 18:10:09 +0100

    fixed waitlistProperty error for junction table, consolelogged error message

--- app/controller/propertyController.js
+++ app/controller/propertyController.js
@@ -198,6 +198,7 @@ const joinPropertyWaitlist = async (req, res) => {
         }
         res.status(404).send("Property is not in waitlist");
     } catch (error) {
+        console.log(error);
         res.status(500).send(error.message);
     }
 };
